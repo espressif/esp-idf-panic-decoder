0a8c83db4df81fb97e30ed172dd77ddabdebef97
Peter Dragun <peter.dragun@espressif.com>
2025-03-07 13:49:23 +0100

    change: Update version to 1.3.0

--- esp_idf_panic_decoder/__init__.py
+++ esp_idf_panic_decoder/__init__.py
@@ -1,4 +1,4 @@
 from esp_idf_panic_decoder.pc_address_decoder import PcAddressDecoder  # noqa: F401
 from esp_idf_panic_decoder.panic_output_decoder import PanicOutputDecoder  # noqa: F401
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
